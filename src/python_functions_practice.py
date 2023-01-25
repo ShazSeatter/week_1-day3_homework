1b96bec03e56a62504bc188331065d09ffd38aff
Shazreen Seatter <shazseatter12@gmail.com>
2023-01-25 20:16:14 +0000

    answered last test questions

--- src/python_functions_practice.py
+++ src/python_functions_practice.py
@@ -18,12 +18,11 @@ def divide(num_1, num_2):
 def length_of_string(string):
     return len(string)
 
-#  -------------
+
 
 def join_string( str_1, str_2):
     return str_1 + str_2
 
-#  -------------
 
 def add_string_as_number(str_1, str_2):
     str_1 = int(str_1)
@@ -34,8 +33,19 @@ def add_string_as_number(str_1, str_2):
 def number_to_full_month_name(number):
     if number == 1:
         return "January"
+    if number == 3:
+        return "March"
+    elif number == 9:
+        return "September"
 
 
+def number_to_short_month_name(month_num):
+    if month_num == 1:
+        return "Jan"
+    if month_num == 4:
+        return "Apr"
+    elif month_num == 10:
+        return "Oct"   
 
 
 
