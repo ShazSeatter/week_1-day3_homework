f5fd6802ff4bc46707210b13eb3d6706ec85d824
Shazreen Seatter <shazseatter12@gmail.com>
2023-01-25 15:33:10 +0000

    first commit

---
+++ src/python_functions_practice.py
@@ -0,0 +1,23 @@
+def return_10():
+    return 10
+
+def add(num_1, num_2):
+    return num_1 + num_2
+
+def subtract(num_1, num_2):
+    return num_1 - num_2
+
+def multiply(num_1, num_2):
+    return num_1 * num_2
+
+
+def divide(num_1, num_2):
+    return num_1 // num_2
+
+
+def length_of_string(test_string):
+    len(test_string)
+
+
+
+
