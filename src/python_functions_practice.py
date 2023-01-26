893287afe58c8a92a35c82631566549708dfb7ba
Shazreen Seatter <shazseatter12@gmail.com>
2023-01-26 09:45:01 +0000

    added some comments and changes

--- src/python_functions_practice.py
+++ src/python_functions_practice.py
@@ -1,23 +1,26 @@
 def return_10():
     return 10
 
+
 def add(num_1, num_2):
     return num_1 + num_2
 
+
 def subtract(num_1, num_2):
     return num_1 - num_2
+
 
 def multiply(num_1, num_2):
     return num_1 * num_2
 
 
+# floors result 
 def divide(num_1, num_2):
     return num_1 // num_2
 
 
 def length_of_string(string):
     return len(string)
-
 
 
 def join_string( str_1, str_2):
@@ -29,11 +32,11 @@ def add_string_as_number(str_1, str_2):
     str_2 = int(str_2)
     return str_1 + str_2
 
-
+#  if there are multiple outcomes, should test for all the possible situations
 def number_to_full_month_name(number):
     if number == 1:
         return "January"
-    if number == 3:
+    elif number == 3:
         return "March"
     elif number == 9:
         return "September"
@@ -42,7 +45,7 @@ def number_to_full_month_name(number):
 def number_to_short_month_name(month_num):
     if month_num == 1:
         return "Jan"
-    if month_num == 4:
+    elif month_num == 4:
         return "Apr"
     elif month_num == 10:
         return "Oct"   
